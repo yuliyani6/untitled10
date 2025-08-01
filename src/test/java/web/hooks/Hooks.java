b102b182152598816b99ad383acc365cad3fc948
yuliyani6 <yuliyaniaprilyani96@gmail.com>
2025-08-01 20:40:07 +0700

    add

--- src/test/java/web/hooks/Hooks.java
+++ src/test/java/web/hooks/Hooks.java
@@ -4,14 +4,26 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
     public static WebDriver driver;
 
     @Before
     public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+
+        // Deteksi OS Windows, hanya gunakan user-data-dir di laptop pribadi
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            options.addArguments("--user-data-dir=C:/Users/YOUR_USERNAME/AppData/Local/Google/Chrome/User Data");
+        }
+
+        // Supaya bisa jalan di GitHub Actions
+        options.addArguments("--headless=new");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+
+        driver = new ChromeDriver(options);
     }
 
     @After
@@ -20,4 +32,5 @@ public class Hooks {
             driver.quit();
         }
     }
+
 }
